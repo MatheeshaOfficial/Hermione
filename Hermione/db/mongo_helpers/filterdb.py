23f9d7788248617431b4320efbeb5821d5a7967e
Matheesha Lestrang <77558226+MatheeshaOfficial@users.noreply.github.com>
2022-07-19 02:54:35 -0700

    Hermione

---
+++ Hermione/db/mongo_helpers/filterdb.py
@@ -0,0 +1,77 @@
+from typing import Dict, List, Union
+
+from Hermione.services.mongo2 import db
+
+# Ported from https://github.com/TheHamkerCat/WilliamButcherBot
+"""
+MIT License
+Copyright (c) 2021 TheHamkerCat
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+filtersdb = db.filters
+
+
+""" Filters funcions """
+
+
+async def _get_filters(chat_id: int) -> Dict[str, int]:
+    _filters = await filtersdb.find_one({"chat_id": chat_id})
+    if _filters:
+        _filters = _filters["filters"]
+    else:
+        _filters = {}
+    return _filters
+
+
+async def get_filters_names(chat_id: int) -> List[str]:
+    _filters = []
+    for _filter in await _get_filters(chat_id):
+        _filters.append(_filter)
+    return _filters
+
+
+async def get_filter(chat_id: int, name: str) -> Union[bool, dict]:
+    name = name.lower().strip()
+    _filters = await _get_filters(chat_id)
+    if name in _filters:
+        return _filters[name]
+    else:
+        return False
+
+
+async def save_filter(chat_id: int, name: str, _filter: dict):
+    name = name.lower().strip()
+    _filters = await _get_filters(chat_id)
+    _filters[name] = _filter
+
+    await filtersdb.update_one(
+        {"chat_id": chat_id}, {"$set": {"filters": _filters}}, upsert=True
+    )
+
+
+async def delete_filter(chat_id: int, name: str) -> bool:
+    filtersd = await _get_filters(chat_id)
+    name = name.lower().strip()
+    if name in filtersd:
+        del filtersd[name]
+        await filtersdb.update_one(
+            {"chat_id": chat_id}, {"$set": {"filters": filtersd}}, upsert=True
+        )
+        return True
+    return False
---
+++ Hermione/helper_extra/blogo_helper.py
@@ -0,0 +1,50 @@
+from bs4 import *
+import shutil
+import requests
+import os
+import base64
+import sys
+import random
+import requests
+
+def download_images(images): 
+    count = 0
+    print(f"Total {len(images)} Image Found!") 
+    if len(images) != 0:
+        for i, image in enumerate(images):
+            try:
+                image_link = image["data-srcset"] 
+            except: 
+                try: 
+                    image_link = image["data-src"] 
+                except:
+                    try:
+                        image_link = image["data-fallback-src"] 
+                    except:
+                        try:
+                            image_link = image["src"] 
+                        except: 
+
+                            pass
+            try: 
+                r = requests.get(image_link).content 
+                try:
+
+                    r = str(r, 'utf-8')
+                except UnicodeDecodeError:
+                    with open("logo@HermioneSLBot.jpg", "wb+") as f: 
+                        f.write(r)
+                    count += 1
+            except: 
+                pass
+
+
+def mainne(name, typeo):
+    url = f"https://www.brandcrowd.com/maker/logos?text={name}&searchtext={typeo}&searchService="
+    r = requests.get(url) 
+    soup = BeautifulSoup(r.text, 'html.parser') 
+    images = soup.findAll('img') 
+    random.shuffle(images)
+    if images is not None:
+       print("level 1 pass")
+    download_images(images)
